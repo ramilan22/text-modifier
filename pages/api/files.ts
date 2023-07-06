2cf7c581d88b024b339a5e0bbc3db8b1162552c0
rami babay <ramibabay21@gmail.com>
2023-07-06 13:01:32 +0100

    post

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -10,33 +10,36 @@ export default function handler(req:NextApiRequest, res:NextApiResponse) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   
   // Handle preflight request
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+  
+  if (req.method === 'POST'){
+
+    
+    
+
+    const directoryPaths = req.body;
+    console.log(directoryPaths)
+    // Replace with the actual directory path
+    const directoryPath=directoryPaths.inputValue
+    
+    
+    
+    
+    try{
+      const files = fs.readdirSync(directoryPath);
+      
+      files.forEach((file) => {
+        const oldFilePath = path.join(directoryPath, file);
+        const newFileName = file.replace(/\s+/g, '-');
+        const newFilePath = path.join(directoryPath, newFileName);
+        
+        fs.renameSync(oldFilePath, newFilePath);
+      });
+      
+      console.log('File names changed successfully!');
+      res.status(200).json({files})
+    } catch (error) {
+      console.error('Error changing file names:', error);
+    }
+  }
   }
   
-  const directoryPaths = req.body;
-  console.log(directoryPaths)
-   // Replace with the actual directory path
-   const directoryPath=directoryPaths.inputValue
-   
-
-   
-   
-   try{
-  const files = fs.readdirSync(directoryPath);
-
-    files.forEach((file) => {
-      const oldFilePath = path.join(directoryPath, file);
-      const newFileName = file.replace(/\s+/g, '-');
-      const newFilePath = path.join(directoryPath, newFileName);
-
-      fs.renameSync(oldFilePath, newFilePath);
-    });
-
-    console.log('File names changed successfully!');
-    res.status(200).json({files})
-  } catch (error) {
-    console.error('Error changing file names:', error);
-  }
-}
